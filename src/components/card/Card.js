2b0db09e79632ab35344bd0ce2bdba02dd0eb130
Logesh <logeshkrishna08@gmail.com>
2023-04-12 12:39:04 +0530

    completed database integration

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './CardContainer.css';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const CardContainer = () => {
-    const cards = [
-      { id: 0, title: 'Overall', content: '+xxXP' },
-      { id: 1, title: 'Container 1', content: '+xxXP' },
-      { id: 2, title: 'Container 2', content: '+xxXP' },
-      { id: 3, title: 'Container 3', content: '+xxXP' },
-      { id: 4, title: 'Container 4', content: '+xxXP' },
-      { id: 5, title: 'Container 5', content: '+xxXP' },
-    ];
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+      const dbRef = ref(database, 'containers/');
+      onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+        const newCards = [];
+        let total = 0;
+  
+        for (let key in data) {
+          if (data.hasOwnProperty(key)) {
+            newCards.push({
+              id: key,
+              title: key,
+              content: `+${data[key].xp} XP`,
+              failed: data[key].failed
+            });
+            total += data[key].xp;
+          }
+        }
+        newCards.unshift({
+            id: 0,
+            title: 'Overall Total',
+            content: `+${total} XP`
+        });
+        setCards(newCards);
+      });
+    }, []);
   
     return (
       <div className="card-container">
